38e3844930b3d710c39f8bd78d4d63f51be6b3f9
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-27 22:13:14 +0100

    Add game route

---
+++ src/routes/endpoints/game.ts
@@ -0,0 +1,19 @@
+import type { RequestHandler } from '@sveltejs/kit'
+import { handleFetchError, catched } from '../../lib/fetch'
+import endpoints from '../../endpoints'
+
+const getGame = async (token:string) => {
+    return fetch(endpoints.group, {
+        method: "GET",
+        headers: { authorization: `Bearer ${token}`}
+    }).then(handleFetchError).then(res => res.json())
+}
+
+export const get: RequestHandler = async (req) => {
+    return await catched(async () => {
+    const data = await getGame(req.locals.accessToken)
+    return {
+        body: data
+    }
+    })
+}
