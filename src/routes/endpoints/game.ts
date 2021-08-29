704827fa0eab231bc171ab16be9b7ec82c20f7ca
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-08-29 09:23:53 +0100

    Fix path for fetching game

--- src/routes/endpoints/game.ts
+++ src/routes/endpoints/game.ts
@@ -3,7 +3,7 @@ import { handleFetchError, catched } from '../../lib/fetch'
 import endpoints from '../../endpoints'
 
 const getGame = async (token:string) => {
-    return fetch(endpoints.group, {
+    return fetch(endpoints.game, {
         method: "GET",
         headers: { authorization: `Bearer ${token}`}
     }).then(handleFetchError).then(res => res.json())
