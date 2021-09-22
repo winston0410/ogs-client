1ee2cb1595f412f4942ca92fb27a3b3c27e2e38a
kghugo <hugosum.dev@protonmail.com>
2021-09-22 19:02:43 +0100

    Get the latest page

--- src/routes/endpoints/game.ts
+++ src/routes/endpoints/game.ts
@@ -1,20 +1,35 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import endpoints from '../../endpoints';
 import type { IGames } from '$lib/typing';
-import createFetch from "wrapped-fetch"
-import type {UnwrappedResponse} from "wrapped-fetch"
+import createFetch from 'wrapped-fetch';
+import type { UnwrappedResponse } from 'wrapped-fetch';
 
 const getGame = async (token: string): Promise<UnwrappedResponse<IGames>> => {
-    const f = createFetch()
-	return f(endpoints.game, {
+	const f = createFetch();
+    const fetchOpts = {
 		method: 'GET',
 		headers: { authorization: `Bearer ${token}` }
-	})
+	}
+	const res = await f<IGames>(endpoints.game, fetchOpts);
+
+    if (!res.ok) {
+        return res
+    }
+
+    //  Check if there is next page
+    if (!res.body.next) {
+        return res
+    }
+
+    const pageCount = Math.ceil(res.body.count / 10)
+
+    return f<IGames>(`${endpoints.game}?page=${pageCount}`, fetchOpts)
 };
 
 export const get: RequestHandler = async (req) => {
 	try {
 		const data = await getGame(req.locals.accessToken);
+		console.log('check get game result', data);
 		return {
 			body: data.body
 		};
